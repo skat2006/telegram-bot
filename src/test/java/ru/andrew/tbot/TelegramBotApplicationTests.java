3807d7f77317a1d0c86c5d384fa1aa6c5db287c9
Andrew <skat2006@bk.ru>
2022-12-10 22:41:41 +0300

    Initial commit

---
+++ src/test/java/ru/andrew/tbot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andrew.tbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
